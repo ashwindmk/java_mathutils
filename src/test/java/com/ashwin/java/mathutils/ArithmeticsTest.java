a7172b4731dd4c40a82eccdbebf9f51bd3266d7a
Ashwin Dinesh <ashwin.dinesh01@gmail.com>
2019-11-02 19:27:31 +0530

    Deploy initial code

---
+++ src/test/java/com/ashwin/java/mathutils/ArithmeticsTest.java
@@ -0,0 +1,11 @@
+package com.ashwin.java.mathutils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArithmeticsTest {
+    /*@Test
+    public void testAdd() {
+        Assert.assertEquals("add test", Arithmetics.add(12.0, 3), 15.0);
+    }*/
+}
